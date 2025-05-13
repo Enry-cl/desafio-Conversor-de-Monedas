4787a9caae0f1c36ea4a427246320d327c18e748
Enry-cl <enry.cl@gmail.com>
2025-05-13 10:54:04 -0400

    Initial commit

---
+++ src/TipoDeMonedaNoEncontradaException.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+public class TipoDeMonedaNoEncontradaException extends RuntimeException {
+  public TipoDeMonedaNoEncontradaException(String message) {
+    super(message);
+  }
+}
